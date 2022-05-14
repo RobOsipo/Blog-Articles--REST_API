2aa47a8d7b15272f1a22e4bceff4f5d28d0efa11
Robert <osipovitchr@gmail.com>
2022-05-14 13:52:42 -0400

    GET PUT DELETE at /article/:articleTitle

--- app.js
+++ app.js
@@ -29,37 +29,105 @@ app.get('/', (req, res) => {
     res.send('<h1>Welcome to the Coding Wiki API</h1>')
 })
 
-app.get('/articles', (req, res) => {
-    Article.find((err, articles) => {
-        if(err) {return res.send(err)}
-        res.send(articles)
+// Request for all articles, GET all articles, POST to all articles, DELETE all articles
+app.route("/articles")
+    .get( (req, res) => {
+            Article.find((err, articles) => {
+                if(err) {return res.send(err)}
+                res.send(articles)
+            })
+        })
+    .post((req, res) => {
+            const {content, title} = req.body
+        
+        
+            const newArticle = new Article({
+                title: title,
+                content: content
+            })
+        
+            newArticle.save((err) => {
+                if (!err) {res.send('Successfully added a new article')} else {res.send(err)}
+            })
+        })
+    .delete((req, res) => {
+            Article.deleteMany({}, (err) => {
+                if (!err) {
+                    res.send('Successfully deleted all articles')
+                } else {
+                    res.send(err)
+                }
+            })
     })
-})
 
-app.post('/articles', (req, res) => {
-    const {content, title} = req.body
+// Request for a single article, GET one article, PUT one article,  DELETE one article
 
-
-    const newArticle = new Article({
-        title: title,
-        content: content
+app.route("/articles/:articleTitle")
+    .get((req, res) => {
+        const {articleTitle} = req.params
+        Article.findOne({ title: articleTitle  }, (err, foundArticle) => {
+            if (foundArticle) {
+                res.send(foundArticle)
+            } else {
+                res.send('No articles matching that Title was found')
+            }
+        })
+    })
+    .put((req, res) => {
+        const {articleTitle} = req.params
+        const {title, content} = req.body
+        Article.updateOne(
+            { title: articleTitle }, 
+            {title: title, content: content}, 
+            (err) => {
+                if (!err) {res.send(`succesfully updated ${articleTitle}`)}
+                if (err) {res.send('It goofed')}
+            }
+        )
+    })
+    .delete((req, res) => {
+        const {articleTitle} = req.params
+        Article.deleteOne(
+            { title: articleTitle },
+            (err) => {
+                if (!err) {
+                    res.send(`Succesfully deleted ${articleTitle}`)
+                }
+            }
+        )
     })
 
-    newArticle.save((err) => {
-        if (!err) {res.send('Successfully added a new article')} else {res.send(err)}
-    })
-})
+// app.get('/articles', (req, res) => {
+//     Article.find((err, articles) => {
+//         if(err) {return res.send(err)}
+//         res.send(articles)
+//     })
+// })
+
+// app.post('/articles', (req, res) => {
+//     const {content, title} = req.body
 
 
-app.delete('/articles', (req, res) => {
-    Article.deleteMany({}, (err) => {
-        if (!err) {
-            res.send('Successfully deleted all articles')
-        } else {
-            res.send(err)
-        }
-    })
-})
+//     const newArticle = new Article({
+//         title: title,
+//         content: content
+//     })
+
+//     newArticle.save((err) => {
+//         if (!err) {res.send('Successfully added a new article')} else {res.send(err)}
+//     })
+// })
+
+
+// app.delete('/articles', (req, res) => {
+//     Article.deleteMany({}, (err) => {
+//         if (!err) {
+//             res.send('Successfully deleted all articles')
+//         } else {
+//             res.send(err)
+//         }
+//     })
+// })
 
 
 app.listen(5000, () => {
